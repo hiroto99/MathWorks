cc1ba79edf1f85588fdc0feceb160023b24d77f1
hiroto99 <107304676+hiroto99@users.noreply.github.com>
2023-10-21 02:21:16 +0000

    Update to positiondata.

--- mathworks/positiondata.py
+++ mathworks/positiondata.py
@@ -4,63 +4,65 @@ import time
 # 座標に関するクラスです
 class Position:
     # 座標の変数を設定します
-    def __init__(self, pos:list, _x:int, _y:int):
-        self.pos = pos
+    def __init__(self, _x:int, _y:int):
         self._x = _x
         self._y = _y
 
     # リストを座標の変数に変換します
-    def NewPosition(self):
-        if (self.pos[0] != "?listtype=__pos__"):
-            self.pos = ["?listtype=__pos__"]
-        return self.pos
+    def NewPosition(self, pos:list):
+        if (pos == None):
+            pos = ["?listtype=__pos__"]
+        else:
+            if (pos[0] != "?listtype=__pos__"):
+                pos = ["?listtype=__pos__"]
+        return pos
     
     # 座標の変数をリストに戻します
-    def DeletePosition(self):
-        if (self.pos[0] == "listtype=__pos__"):
-            self.pos = []
-        return self.pos
+    def DeletePosition(self, pos:list):
+        if (pos[0] == "?listtype=__pos__"):
+            pos = []
+        return pos
     
     # 座標の変数であるかを出力します
-    def IsPositionVar(self):
-        if (self.pos[0] == "listtype=__pos__"):
+    def IsPositionVar(self, pos:list):
+        if (pos[0] == "?listtype=__pos__"):
             print("True")
         else:
             print("False")
     
     # 座標を追加します
-    def add(self):
+    def add(self, pos:list):
         adda = [self._x, self._y]
-        if (self.pos[0] == "listtype=__pos__"):
-            if (self.pos not in adda):
-                self.pos.append(adda)
+        if (pos[0] == "?listtype=__pos__"):
+            if (pos not in adda):
+                pos.append(adda)
         else:
             raise TypeError("can only concatenate list (not 'position') to position")
-        return self.pos
+        return pos
     
     # 座標を削除します
-    def delete(self):
+    def delete(self, pos:list):
         dela = [self._x, self._y]
-        self.pos.remove(dela)
-        return self.pos
+        pos.remove(dela)
+        return pos
     
     # 座標の要素を取得します
-    def GetPos(self, plece:int):
-        _x = self.pos[plece][0]
-        _y = self.pos[plece][1]
+    def GetPos(self, plece:int, pos:list):
+        _x = pos[plece][0]
+        _y = pos[plece][1]
         return [_x, _y]
 
     # 座標の場所を求めます
-    def FindPlece(self):
+    def FindPlece(self, pos:list):
         FindA = [self._x, self._y]
-        FindB = self.pos.index(FindA)
+        FindB = pos.index(FindA)
         if (FindB != -1):
             return FindB
         else:
             return None
     
     # 座標をjsonファイルとして出力します
-    def ExportFile(self, path):
+    def ExportFile(self, path, pos:list):
         wait = time.sleep
         paths = path + ".json"
         if (os.path.exists(paths)):
@@ -73,8 +75,8 @@ class Position:
                 print("Opening Files...")
                 with open(paths, 'w', encoding='utf-8') as file:
                     print("Writeing...")
-                    for A in range(len(self.pos)):
-                        file.write(self.pos[A])
+                    for A in range(len(pos)):
+                        file.write(pos[A])
                 print("Finish!")
             else:
                 print("File export failed.")
@@ -84,6 +86,6 @@ class Position:
                 print("New files was Create.")
                 wait(0.5)
                 print("Writeing...")
-                for A in range(len(self.pos)):
-                    file.write(self.pos[A])
+                for A in range(len(pos)):
+                    file.write(pos[A])
             print("Finish!")
